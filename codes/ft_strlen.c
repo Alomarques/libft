14695116580a88a991ace43f4571eafd59ad1a30
aloisiomarques <aloisiomojunior@gmail.com>
2024-10-13 12:19:12 +0200

    add files 2

--- codes/ft_strlen.c
+++ codes/ft_strlen.c
@@ -1,11 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aloisiojr <aloisiojr@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/10 15:36:19 by aloisiojr         #+#    #+#             */
-/*   Updated: 2024/10/10 15:36:48 by aloisiojr        ###   ########.fr       */
+/*   Created: 2024/10/10 18:00:27 by aloisiojr         #+#    #+#             */
+/*   Updated: 2024/10/12 10:18:35 by aloisiojr        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../libft.h"
+
+size_t  ft_strlen(const char *s)
+{
+  size_t length = 0;
+  while (s[length] != '\0')
+    length++;
+  return (length);
+}
